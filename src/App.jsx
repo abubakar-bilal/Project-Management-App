d6a9db5e4de221e74e6798d97877650f4ba8f2a6
Abubaker Bilal <abubaker031@gmail.com>
2024-10-21 11:39:39 +0500

    Project management App with React concepts

---
+++ src/App.jsx
@@ -0,0 +1,125 @@
+import NewProject from "./components/NewProject";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
+import SelectedProject from "./components/SelectedProject";
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+    tasks: [],
+  });
+
+  function handleAddTask(taskText) {
+    setProjectsState((prevState) => {
+      const newTask = {
+        text: taskText,
+        id: Math.random(),
+        projectId: prevState.selectedProjectId,
+      };
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== taskId),
+      };
+    });
+  }
+
+  function handleStartAddProject() {
+    setProjectsState((prevProjectState) => {
+      return {
+        ...prevProjectState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleCancelAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  function handleSelectedProject(projectId) {
+    setProjectsState((prevProjectState) => {
+      return {
+        ...prevProjectState,
+        selectedProjectId: projectId,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    const newProject = {
+      ...projectData,
+      id: Math.random(),
+    };
+
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
+  let content = (
+    <SelectedProject
+      onDelete={handleDeleteProject}
+      project={selectedProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
+  );
+  if (projectsState.selectedProjectId === null) {
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
+  } else if (projectsState.selectedProjectId == undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar
+        projects={projectsState.projects}
+        onStartAddProject={handleStartAddProject}
+        onSelectProject={handleSelectedProject}
+        selectedProjectId={projectsState.selectedProjectId}
+      />
+      {content}
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/NewProject.jsx
@@ -0,0 +1,73 @@
+import { useRef } from "react";
+import Input from "./Input.jsx";
+import Modal from "./Modal.jsx";
+
+export default function NewProject({ onAdd, onCancel }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  const modalRef = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDesc = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    // validate
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDesc.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modalRef.current.open();
+      return;
+    }
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDesc,
+      dueDate: enteredDueDate,
+    });
+  }
+
+  return (
+    <>
+      <div className="w-[35rem] mt-16 ">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              onClick={onCancel}
+              className="text-stone-800 hover:text-stone-950"
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" isTextArea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
+      </div>
+      <Modal ref={modalRef} buttonCaption="Close">
+        <h2 className="text-xl font-bold text-stone-800 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops... look like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field
+        </p>
+      </Modal>
+    </>
+  );
+}
